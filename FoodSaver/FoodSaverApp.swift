af99ee5459bca3f007a8d5f28db1deee5a5e174e
Pat Brown <patrickdeanbrown@icloud.com>
2024-07-11 16:55:08 -0700

    Initial Commit

---
+++ FoodSaver/FoodSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSaverApp.swift
+//  FoodSaver
+//
+//  Created by Patrick Brown on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
